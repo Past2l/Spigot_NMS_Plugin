19c773c5748b54fb043399fe4c4591485d0f820d
Dohyeon Yun <to@dohyun.it>
2023-11-22 23:55:48 +0900

    feat: add `NMS` api

--- plugin/src/main/java/it/dohyun/plugin/Plugin.kt
+++ plugin/src/main/java/it/dohyun/plugin/Plugin.kt
@@ -1,10 +1,12 @@
 package it.dohyun.plugin
 
+import it.dohyun.plugin.api.util.NMS
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
+        if (!NMS.init()) return
     }
 
     override fun onDisable() {
