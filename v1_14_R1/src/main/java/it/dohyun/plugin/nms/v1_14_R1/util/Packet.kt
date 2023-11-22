da665696b80c5aa69e2a6668bd53e0b2c102b2b2
Dohyeon Yun <to@dohyun.it>
2023-11-22 23:50:22 +0900

    feat: add `Packet` setTabList, getValue, setValue

--- v1_14_R1/src/main/java/it/dohyun/plugin/nms/v1_14_R1/util/Packet.kt
+++ v1_14_R1/src/main/java/it/dohyun/plugin/nms/v1_14_R1/util/Packet.kt
@@ -1,7 +1,34 @@
 package it.dohyun.plugin.nms.v1_14_R1.util
 
 import it.dohyun.plugin.api.type.nms.Packet
+import net.minecraft.server.v1_14_R1.IChatBaseComponent
+import net.minecraft.server.v1_14_R1.PacketPlayOutPlayerListHeaderFooter
 import org.bukkit.plugin.Plugin
 
 class Packet(val plugin: Plugin) : Packet {
+    override fun setTabList(header: String, footer: String): Any {
+        val packet = PacketPlayOutPlayerListHeaderFooter()
+        packet.header = IChatBaseComponent.ChatSerializer.a("""{"text":"${header.replace("\n", "\\n")}"}""")
+        packet.footer = IChatBaseComponent.ChatSerializer.a("""{"text":"${footer.replace("\n", "\\n")}"}""")
+        return packet
+    }
+
+    private fun getValue(instance: Any, name: String): Any? {
+        var result: Any? = null
+        try {
+            val field = instance.javaClass.getDeclaredField(name)
+            field.isAccessible = true
+            result = field.get(instance)
+            field.isAccessible = false
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
+        return result
+    }
+
+    private fun setValue(obj: Any, key: String, value: Any?) {
+        val field = obj.javaClass.getDeclaredField(key)
+        field.isAccessible = true
+        field.set(obj, value)
+    }
 }
