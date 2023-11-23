ec21b39ae5fcdd589d98e068b05319ec19f59ab8
Dohyeon Yun <to@dohyun.it>
2023-11-23 11:23:03 +0900

    refactor: remove `Packet` class

--- v1_16_R3/src/main/java/it/dohyun/nms/nms/v1_16_R3/util/NMS.kt
+++ v1_16_R3/src/main/java/it/dohyun/nms/nms/v1_16_R3/util/NMS.kt
@@ -1,20 +1,42 @@
 package it.dohyun.nms.nms.v1_16_R3.util
 
 import it.dohyun.nms.api.type.nms.NMS
+import net.minecraft.server.v1_16_R3.IChatBaseComponent
 import net.minecraft.server.v1_16_R3.Packet
+import net.minecraft.server.v1_16_R3.PacketPlayOutPlayerListHeaderFooter
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 
 class NMS(plugin: Plugin) : NMS {
-    private val packet = Packet(plugin)
-
     override fun sendPacket(player: Player, packet: Any) {
         val connection = (player as CraftPlayer).handle.playerConnection
         connection.sendPacket(packet as Packet<*>)
     }
 
     override fun setTabList(player: Player, header: String, footer: String) {
-        sendPacket(player, packet.setTabList(header, footer))
+        val packet = PacketPlayOutPlayerListHeaderFooter()
+        packet.header = IChatBaseComponent.ChatSerializer.a("""{"text":"${header.replace("\n", "\\n")}"}""")
+        packet.footer = IChatBaseComponent.ChatSerializer.a("""{"text":"${footer.replace("\n", "\\n")}"}""")
+        sendPacket(player, packet)
+    }
+
+    private fun getValue(instance: Any, name: String): Any? {
+        var result: Any? = null
+        try {
+            val field = instance.javaClass.getDeclaredField(name)
+            field.isAccessible = true
+            result = field.get(instance)
+            field.isAccessible = false
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
+        return result
+    }
+
+    private fun setValue(obj: Any, key: String, value: Any?) {
+        val field = obj.javaClass.getDeclaredField(key)
+        field.isAccessible = true
+        field.set(obj, value)
     }
 }
--- v1_20_R1/src/main/java/it/dohyun/nms/nms/v1_20_R1/util/NMS.kt
+++ v1_20_R1/src/main/java/it/dohyun/nms/nms/v1_20_R1/util/NMS.kt
@@ -1,20 +1,46 @@
 package it.dohyun.nms.nms.v1_20_R1.util
 
 import it.dohyun.nms.api.type.nms.NMS
+import net.minecraft.network.chat.MutableComponent
+import net.minecraft.network.chat.contents.LiteralContents
 import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.ClientboundTabListPacket
 import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 
 class NMS(plugin: Plugin) : NMS {
-    private val packet = Packet(plugin)
-
     override fun sendPacket(player: Player, packet: Any) {
         val connection = (player as CraftPlayer).handle.connection
         connection.send(packet as Packet<*>)
     }
 
     override fun setTabList(player: Player, header: String, footer: String) {
-        sendPacket(player, packet.setTabList(header, footer))
+        sendPacket(
+            player,
+            ClientboundTabListPacket(
+                MutableComponent.create(LiteralContents(header)),
+                MutableComponent.create(LiteralContents(footer))
+            )
+        )
+    }
+
+    private fun getValue(instance: Any, name: String): Any? {
+        var result: Any? = null
+        try {
+            val field = instance.javaClass.getDeclaredField(name)
+            field.isAccessible = true
+            result = field.get(instance)
+            field.isAccessible = false
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
+        return result
+    }
+
+    private fun setValue(obj: Any, key: String, value: Any?) {
+        val field = obj.javaClass.getDeclaredField(key)
+        field.isAccessible = true
+        field.set(obj, value)
     }
 }
